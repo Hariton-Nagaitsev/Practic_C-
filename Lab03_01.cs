a760cd9ef8ab214870b792943b8ee92c88df353c
Hariton-Nagaitsev <148050661+Hariton-Nagaitsev@users.noreply.github.com>
2023-11-19 17:35:48 +0300

    Create Lab03_01.cs

---
+++ Lab03_01.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WhatDay1
+{
+    enum MonthName
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+    class WhatDay
+    {
+        static System.Collections.ICollection DaysInMonths = new int[12] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a day number between 1 and 365: ");
+            int dayNum = int.Parse(Console.ReadLine());
+            int monthNum = 0;
+
+            foreach (int daysInMonth in DaysInMonths)
+            {
+                if (dayNum <= daysInMonth)
+                {
+                    break;
+                }
+                else
+                {
+                    dayNum -= daysInMonth;
+                    monthNum++;
+                }
+            }
+            MonthName temp = (MonthName)monthNum;
+            string monthName = temp.ToString();
+            Console.WriteLine("{0} {1}", dayNum, monthName);
+            
+        }
+    }
+}
