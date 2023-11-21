a7cb2c19d4a32953bbb401dc318e02eda1a733ad
Hariton-Nagaitsev <148050661+Hariton-Nagaitsev@users.noreply.github.com>
2023-11-21 18:47:21 +0300

    Create Sum.cs

---
+++ Sum.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+
+namespace Sum
+{
+
+	/// Контрольное задание - Вычислить сумму цифр числа. Обосновать выбор типа цикла
+    class Program
+    {
+        static void Main()
+        {
+          int num = int.Parse(Console.ReadLine());
+          int summ = 0;
+          int mult = 1;
+          int d;
+          
+          while (num > 0)
+          {
+            d = num % 10;
+			      suma = summ + d;
+			      num = num / 10;
+          }
+		      Console.WriteLine(summ);
+        }
+    }
+}
