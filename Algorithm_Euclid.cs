aeefe194dd38d6cad3beb140c1df838050da300f
Hariton-Nagaitsev <148050661+Hariton-Nagaitsev@users.noreply.github.com>
2023-11-21 19:26:23 +0300

    Update Algorithm_Euclid.cs

--- Algorithm_Euclid.cs
+++ Algorithm_Euclid.cs
@@ -1,3 +1,4 @@
+// Вариант 1
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,22 +8,22 @@ namespace Eucl
 {
 	public class Program
 	{
-	  public static int Euclid(int a, int b, out int nod)
-	  {
-	    while (a != b)
-	    {
-	      if (a > b)
-	      {
-	        a = a - b;
-	      }
-	      else
-	      {
-	        b = b - a;
-	      }
-	    }
-	    nod = b;
-	    return nod;
-	  }
+	  	public static int Euclid(int a, int b, out int nod)
+	  	{
+	    		while (a != b)
+	    		{
+	     	 	if (a > b)
+	      		{
+	        	a = a - b;
+	      		}
+			else
+	      		{
+	        	b = b - a;
+	      		}
+	    		}
+	    		nod = b;
+	    		return nod;
+	  	}
 		public static void Main(string[] args)
 		{
 			Console.WriteLine("Enter 1 number");
@@ -37,7 +38,7 @@ namespace Eucl
 }
 
 
-/* 
+// вариант 2
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -49,16 +50,16 @@ namespace Eucl
 	public class Program
  
 	{
-	  public static int Euclid(int a, int b)
-	  {
-	    int nod;
-	    while (a != b)
-	    {
-	      _ = a > b ? a -= b : b -= a;
-	    }
-	    nod = a;
-	    return nod;
-	  }
+	  	public static int Euclid(int a, int b)
+	  	{
+	    		int nod;
+	    		while (a != b)
+	    		{
+	      		_ = a > b ? a -= b : b -= a;
+	    		}
+	    		nod = a;
+	   		return nod;
+	  		}
 		public static void Main(string[] args)
 		{
 			Console.WriteLine("Enter 1 number");
@@ -70,4 +71,36 @@ namespace Eucl
 			Console.WriteLine($"Greatest common divisor = {nod}");
 		}
 	}
-}*/
+}
+
+// вариант 3
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace HelloWorld
+{
+	public class Program
+	{
+		public static int Euclid(ref int a, int b)
+		{
+		 	int nod;
+	    		while (a != b)
+	    		{
+	      		_ = a > b ? a -= b : b -= a;
+	    		}
+	    		}
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Enter 1 number");
+			int a = int.Parse(Console.ReadLine());
+			Console.WriteLine("Enter 2 number");       
+			int b = int.Parse(Console.ReadLine());
+			
+			Euclid(ref a, b);
+			Console.WriteLine($"Greatest common divisor = {a}");
+		}
+	}
+}
