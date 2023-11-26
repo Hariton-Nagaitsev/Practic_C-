30e43ebfb54761b04d0df70d0ce4c7f2b6780ca3
Hariton-Nagaitsev <148050661+Hariton-Nagaitsev@users.noreply.github.com>
2023-11-26 19:26:15 +0300

    Update AK.cs

--- AK.cs
+++ AK.cs
@@ -15,7 +15,9 @@ namespace Lab04_05
 
             if (a != 0)
             {
-                var r = Sqr(a, b, c, out double x1, out double x2);
+                (double, double, double) result = Sqr(a, b, c);
+                double flag = result.Item1, x1 = result.Item2, x2 = result.Item3;
+                
                 if (x1 == 0 && x2 == 0)
                 {
                     Console.WriteLine("no roots");
@@ -34,9 +36,9 @@ namespace Lab04_05
                 Console.WriteLine("Equation is not square");
             }
         }
-        public static (double, double) Sqr(double a, double b, double c, out double x1, out double x2)
+        public static (double, double, double) Sqr(double a, double b, double c)
         {
-            double r = 0;
+            double flag = 0, x1, x2;
             double d = Math.Pow(b, 2) - 4 * a * c;
             if (d >= 0)
             {
@@ -47,7 +49,7 @@ namespace Lab04_05
                 x1 = 0;
                 x2 = 0;
             }
-            var result = (x1, x2); return result;
+            return (flag, x1, x2);
         }
     }
 }
